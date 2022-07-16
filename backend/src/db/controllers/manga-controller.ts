65d1570f96fa1386a842b10285978faeb81658d9
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2022-07-16 19:26:01 -0400

    remove get decorator

--- backend/src/db/controllers/manga-controller.ts
+++ backend/src/db/controllers/manga-controller.ts
@@ -2,7 +2,6 @@ import { Manga } from "../entities/manga";
 import { mangaDatasource } from "../datasource";
 
 export class MangaController {
-  @Get("/manga")
   getAll() {
     return mangaDatasource.manager.find(Manga);
   }
